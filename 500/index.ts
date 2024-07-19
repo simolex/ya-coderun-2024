f37c8320f68b0c840bedbf94cfa3b82eae1d6b2d
simolex <simolex@yandex.ru>
2024-07-19 22:49:23 +0300

    done task 500

--- 500/index.ts
+++ 500/index.ts
@@ -1,88 +1,82 @@
 type Digit = "0" | "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9";
 
-type Data = string | number;
+type Tuple<L extends number, O extends string = `${L}`, Count extends unknown[] = []> =
+    O extends `${infer F}${infer R}` ? (
+        Tuple<L, R, N<Count>[keyof N & F]>
+    ) : Count
 
-type ParseInteger<
-    S extends string,
-    L extends string = "",
-    Buffer extends string = "",
-    Result extends number[] = []
-> = S extends `${L}${infer C}${any}`
-    ? C extends ""
-        ? Result
-        : C extends Digit
-        ? ParseInteger<S, `${L}${C}`, `${Buffer}${C}`, Result>
-        : Buffer extends ""
-        ? ParseInteger<S, `${L}${C}`, "", [...Result]>
-        : ParseInteger<S, `${L}${C}`, "", [...Result, Num<RemoveLeadingZeros<Buffer>>]>
-    : Buffer extends ""
-    ? Result
-    : [...Result, Num<RemoveLeadingZeros<Buffer>>];
+type N<T extends unknown[] = []> = {
+    '0': [...T, ...T, ...T, ...T, ...T, ...T, ...T, ...T, ...T, ...T],
+    '1': [...T, ...T, ...T, ...T, ...T, ...T, ...T, ...T, ...T, ...T, unknown],
+    '2': [...T, ...T, ...T, ...T, ...T, ...T, ...T, ...T, ...T, ...T, unknown, unknown],
+    '3': [...T, ...T, ...T, ...T, ...T, ...T, ...T, ...T, ...T, ...T, unknown, unknown, unknown],
+    '4': [...T, ...T, ...T, ...T, ...T, ...T, ...T, ...T, ...T, ...T, unknown, unknown, unknown, unknown],
+    '5': [...T, ...T, ...T, ...T, ...T, ...T, ...T, ...T, ...T, ...T, unknown, unknown, unknown, unknown, unknown],
+    '6': [...T, ...T, ...T, ...T, ...T, ...T, ...T, ...T, ...T, ...T, unknown, unknown, unknown, unknown, unknown, unknown],
+    '7': [...T, ...T, ...T, ...T, ...T, ...T, ...T, ...T, ...T, ...T, unknown, unknown, unknown, unknown, unknown, unknown, unknown],
+    '8': [...T, ...T, ...T, ...T, ...T, ...T, ...T, ...T, ...T, ...T, unknown, unknown, unknown, unknown, unknown, unknown, unknown, unknown],
+    '9': [...T, ...T, ...T, ...T, ...T, ...T, ...T, ...T, ...T, ...T, unknown, unknown, unknown, unknown, unknown, unknown, unknown, unknown, unknown],
+}
 
-type TrimStart<S extends string, Start extends string> = S extends `${Start}${infer End}`
-    ? End
-    : "";
-
-type Num<S extends string> = S extends number
-    ? S
-    : S extends `${infer N extends number}`
+type Num<S extends string> = S extends `${infer N extends number}`
     ? number extends N
-        ? never
-        : N
+    ? never
+    : N
     : never;
-
-type Tuple<N, T extends any[] = []> = T["length"] extends N ? T : Tuple<N, [0, ...T]>;
-
-type Sum<A extends number, B extends number> = [...Tuple<A>, ...Tuple<B>]["length"] & number;
-
-type su = Sum<333, 222>;
 
 type RemoveLeadingZeros<T extends string> = T extends "0"
     ? T
     : T extends `${0}${infer Rest}`
     ? RemoveLeadingZeros<Rest>
     : T;
-type q1 = RemoveLeadingZeros<"0001">;
-type q2 = ParseInteger<"2abc000">;
 
-type n = Num<q2>;
-type s = ParseInteger<"00001">;
+type SumTuple<Nums extends number[], O extends unknown[] = [], Index extends unknown[] = [], Count = Nums['length']> =
+    Index['length'] extends Count
+    ? O
+    : SumTuple<Nums, [...O, ...Tuple<Nums[Index['length']]>], [...Index, unknown]>
+
+type ParseInteger<
+    S extends string,
+    L extends string = "",
+    Buffer extends string = "",
+    Result extends number[] = []
+    > = S extends `${L}${infer C}${any}`
+    ? C extends ""
+    ? SumTuple<Result>
+    : C extends Digit
+    ? ParseInteger<S, `${L}${C}`, `${Buffer}${C}`, Result>
+    : Buffer extends ""
+    ? ParseInteger<S, `${L}${C}`, "", [...Result]>
+    : ParseInteger<S, `${L}${C}`, "", [...Result, Num<RemoveLeadingZeros<Buffer>>]>
+    : Buffer extends ""
+    ? SumTuple<Result>
+    : SumTuple<[...Result, Num<RemoveLeadingZeros<Buffer>>]>;
 
 type Prepare<T extends readonly (string | number)[]> = {
-    -readonly [K in keyof T]: T[K] extends string ? ParseInteger<T[K]> : T[K];
+    -readonly [K in keyof T]: T[K] extends string
+    ? ParseInteger<T[K]>
+    : T[K] extends number
+    ? Tuple<T[K]>
+    : [];
 };
 
-type Solution<T extends readonly (string | number)[]> = Solution_<Prepare<T>>;
+type Counter<T extends unknown[], O extends unknown[] = [], Index extends unknown[] = [], Count = T['length']> =
 
-type Solution_<
-    T extends readonly (string | number)[],
-    Vizited extends keyof T = never,
-    Result extends number = 0
-> = Exclude<keyof T, Vizited> extends infer I extends keyof T
-    ? T[I] extends number
-        ? Solution_<T, Vizited | I, Sum<Result, T[I]>>
-        : Solution_<T, Vizited | I, Result> //Sum<Result, Solution<T[I]>>
-    : Result;
-// type Solution<T extends unknown, Result extends number = 0 > =
-//   T extends [infer I, ...infer Rest]
-//     ? I extends number
-//       ? Solution<Rest, Sum<Result, I>>
-//       : I extends string
-//           ? Num< I > extends infer N
+    Index['length'] extends Count
+    ? O['length']
+    : T[Index['length']] extends unknown[]
+    ? Counter<T, [...O, ...T[Index['length']]], [...Index, unknown]>
+    : []
 
-//             ? N extends number
-//               ? Solution<Rest, Sum<Result, N>>
-//               : Solution<Rest, Result >
-//             :Solution<[...Rest, ...ParseInteger<I&string>]>
-//       : Solution<Rest, Result >
-//     : Result;
-
-type eq = Prepare<["4", 1, 1, 2, "2", "3"]>;
-type eq2 = Prepare<e>;
+type Solution<T extends Readonly<Array<string | number>>> = Counter<Prepare<T>>
 
 const arrayOfNumbers = [1, "2abc0", "3"] as const;
 
-type e = typeof arrayOfNumbers;
-
-type weq = Solution<typeof arrayOfNumbers>;
-type weq0 = Solution<["4", 1, 1, 2, "2", "3"]>;
+type a0 = Solution<typeof arrayOfNumbers>;
+type a1 = Solution<typeof arrayOfNumbers>; // 6
+type a2 = Solution<[1, 2, 3, 4, 5, 6]>; // 21
+type a3 = Solution<[1]>; // 1
+type a4 = Solution<["1", "2", "3"]>; // 6
+type a5 = Solution<["1abc", 2, "3"]>; // 6
+type a6 = Solution<[111, 222, 333]>; // 666
+type a7 = Solution<[1, 1, 1, 1, 1, 1, 1, 1, 1, 1/* , ... */]>;
