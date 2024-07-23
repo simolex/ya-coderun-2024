3feadda869da95c486a3d9825d1d789985887294
simolex <simolex@yandex.ru>
2024-07-23 08:54:53 +0300

    save v2 485

--- 485/playground.js
+++ 485/playground.js
@@ -1,49 +1,67 @@
 // helpers
 
-const deletedKeys = Symbol.for("deleted");
-const hiddenIn = Symbol.for("isHidden");
+const objectThis = Symbol("objectThis");
 const deletedSets = new Map();
 
-const initTrigger = function (_deletedKeys, _hiddenIn) {
-    const deletedKeys = _deletedKeys;
-    const hiddenIn = _hiddenIn;
+const initTrigger = function (_objectThis, _deletedSets) {
+    const objectThis = _objectThis;
+    const deletedSets = _deletedSets;
+
     return function () {
-        console.log(Symbol.keyFor(deletedKeys));
-        console.log(Symbol.keyFor(hiddenIn));
+        if (this[objectThis]) {
+            let originalFields = deletedSets.get(this[objectThis]);
+
+            //проверяем,что нас не скопировали
+            if (this[objectThis] !== this) {
+                originalFields = Object.assign({}, originalFields);
+            }
+
+            Object.assign(this, originalFields);
+
+            if (deletedSets.has(this)) {
+                deletedSets.delete(this);
+            }
+            this[objectThis] = undefined;
+            return;
+        }
+
+        this[objectThis] = this;
+        Object.defineProperty(this, objectThis, {
+            enumerable: false,
+        });
+
+        const allKeys = Reflect.ownKeys(this);
+        deletedSets.set(this, {});
+        for (let i = 0; i < allKeys.length; i++) {
+            if (typeof allKeys[i] !== "symbol" && /^\$/.test(allKeys[i]) === false) {
+                deletedSets.get(this)[allKeys[i]] = this[allKeys[i]];
+                delete this[allKeys[i]];
+            }
+        }
     };
 };
 
-const trigger = initTrigger(deletedKeys, hiddenIn);
-console.log(trigger);
+const initGetter = function (_objectThis, _deletedSets) {
+    const objectThis = _objectThis;
+    const deletedSets = _deletedSets;
 
-const trigger_ = function () {
-    // Object.prototype.hasOwn = function () {
-    //     return false;
-    // };
-    if (this[Symbol.for("deleted")] === undefined) {
-        this[Symbol.for("deleted")] = deletedSets;
-        if (this[isHidden]) {
+    return function (key) {
+        let result = this[key];
+
+        if (this[objectThis] !== this) {
+            const originalFields = Object.assign({}, originalFields);
+            deletedSets.set(this, originalFields);
         }
-    }
-    const allKeys = Reflect.ownKeys(this);
-    for (let i = 0; i < allKeys.length; i++) {
-        if (/^\$/.test(allKeys[i]) === false) {
-            const gettedDescriptor = Object.getOwnPropertyDescriptor(this, allKeys[i]);
-            const gettedEnumerable = gettedDescriptor.enumerable;
-            console.log(allKeys[i], gettedEnumerable);
-            Object.defineProperty(this, allKeys[i], {
-                enumerable: gettedEnumerable === undefined ? false : !gettedEnumerable,
-                configurable: gettedEnumerable === undefined ? false : !gettedEnumerable,
-                writable: gettedEnumerable === undefined ? false : !gettedEnumerable
-            });
-            console.log(this.propertyIsEnumerable(allKeys[i]));
+
+        if (!result && this[objectThis]) {
+            result = deletedSets.get(this)[key];
         }
-    }
+        return result;
+    };
 };
-const getter = function (key) {
-    const desc = Object.getOwnPropertyDescriptor(this, key);
-    return desc ? desc.value : undefined;
-};
+
+const trigger = initTrigger(objectThis, deletedSets);
+const getter = initGetter(objectThis, deletedSets);
 
 module.exports = { trigger, getter };
 
@@ -61,12 +79,12 @@ const artObject = {
         return [...stations, ...stops];
     },
     $trigger: trigger,
-    $getter: getter
+    $getter: getter,
 };
-
 artObject.$trigger();
 
 // basic tests
+console.log(artObject);
 console.log(artObject["towers"]);
 console.log("towers" in artObject); //-> false
 console.log(artObject.$getter("towers")); //-> [ 'Oko', 'Neva' ]
