f4256aa0b166a2e12785c2f60f483390f32c2ce6
simolex <simolex@yandex.ru>
2024-07-24 16:18:38 +0300

    save task 497

--- 497/index.test.js
+++ 497/index.test.js
@@ -8,7 +8,9 @@ describe("497. Антивирус", () => {
             { zippedSize: 12000, realSize: 56600 },
             { zippedSize: 105000, vector: "usb_drive" },
         ]);
-        expect(result).toEqual("");
+        expect(result).toEqual(
+            "{severity: 'medium' | 'high', type?: 'spyware', target?: 'winlogon.exe'} | {zippedSize: 12000 | 105000, realSize?: 56600, vector?: 'usb_drive'}"
+        );
     });
     test("test-2", () => {
         const result = solution([
@@ -19,6 +21,15 @@ describe("497. Антивирус", () => {
     });
     test("test-3", () => {
         const result = solution([null, undefined, true, "", 0, {}]);
-        expect(result).toEqual("");
+        expect(result).toEqual("{} | undefined | boolean | '' | null | 0");
+    });
+    test("test-3", () => {
+        const result = solution([
+            { t1: 1, t2: 2, t3: 3 },
+            { t1: 1, t2: 2 },
+            { t1: 1, t3: 3 },
+            { t2: 2, t3: 3 },
+        ]);
+        expect(result).toEqual("{} | undefined | boolean | '' | null | 0");
     });
 });
