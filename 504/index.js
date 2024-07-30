8cdc9247215a1b3f4ba2d7ff31be3676dc700361
simolex <simolex@yandex.ru>
2024-07-30 23:29:01 +0300

    done task 504

--- 504/index.js
+++ 504/index.js
@@ -1,36 +1,22 @@
-const ff = () => {
-    const data = [
-        "ijsoif+-+*-+kmasdligjuweijfsk9890qweasf",
-        "*******jsadvkmasdligjuweijfsk9890qweasf",
-        "ijsoifgjsadvkmasdli%^$^&%$#@*&890qweasf",
-        "ijsoifgjsadvkmasdligjuweijf)()*&^&*%$^&",
-    ];
-    let count = data.length - 1;
-    return function () {
-        const result = { data: data[count] };
-        if (count > 0) count--;
-        return result;
-    };
-};
-const fackFetch = ff();
+// const ff = () => {
+//     const data = [
+//         "ijsoif+-+*-+kmasdligjuweijfsk9890qweasf",
+//         "*******jsadvkmasdligjuweijfsk9890qweasf",
+//         "ijsoifgjsadvkmasdli%^$^&%$#@*&890qweasf",
+//         "ijsoifgjsadvkmasdligjuweijf)()*&^&*%$^&"
+//     ];
+//     let count = data.length - 1;
+//     return function () {
+//         const result = { data: data[count] };
+//         if (count > 0) count--;
+//         return result;
+//     };
+// };
+// const fackFetch = ff();
 
-function solution() {
-    const API_URL = process.env.API_URL;
-    const avaliableLetters = /[a-zA-Z0-9 ]+/g;
-
-    console.log(API_URL);
-
-    const RequestData = async () => {
-        try {
-            const resp = await fetch(API_URL);
-            if (resp.ok) {
-                return await resp.json();
-            }
-        } catch (e) {
-            return { data: "" };
-        }
-    };
-    // const RequestData = () => fackFetch();
+async function solution() {
+    // const API_URL = process.env.API_URL;
+    const avaliableLetters = /[a-zA-Z0-9]+/g;
 
     let match;
     let result = [];
@@ -40,29 +26,27 @@ function solution() {
         const splitLine = line.split("");
 
         for (let i = 0; i < splitLine.length; i++) {
-            if (result[i + at] !== undefined && result[i + at] !== splitLine[i]) {
-                throw "ehuuuu";
-            }
             result[i + at] = splitLine[i];
         }
     };
 
     const checkString = () => result.reduce((sum, v) => (v ? sum + 1 : sum), 0) < lengthString;
 
-    const getData = (resp, key = "data") => (resp !== null ? (resp[key] ? resp[key] : "") : "");
+    let resp;
+    let json;
+    let responseStr;
     do {
-        try {
-            const r = RequestData();
-            const responce = getData(r);
-            if (responce.length > lengthString) {
-                lengthString = responce.length;
-            }
-            console.log(responce);
-            while ((match = avaliableLetters.exec(responce)) !== null) {
-                mergeResult(match.index, match[0]);
-            }
-        } catch (e) {
-            throw new Error(e);
+        resp = await fetch(API_URL).then((resp) => resp.json());
+        json = resp.json();
+
+        responseStr = json.data;
+
+        if (responseStr.length > lengthString) {
+            lengthString = responseStr.length;
+        }
+
+        while ((match = avaliableLetters.exec(responseStr)) !== null) {
+            mergeResult(match.index, match[0]);
         }
     } while (checkString());
 
