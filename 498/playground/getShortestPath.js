077de507e28477b4c30853329ac884f980c8c9e0
simolex <simolex@yandex.ru>
2024-08-18 09:25:40 +0300

    done task 498

--- 498/playground/getShortestPath.js
+++ 498/playground/getShortestPath.js
@@ -36,9 +36,10 @@ function getShortestPath(nodes, edges, startNodeName, endNodeName) {
         const edges1 = edgesByNode1Name[currentNode.name] || [];
         const edges2 = edgesByNode2Name[currentNode.name] || [];
         const currentNodeEdges = [...edges1, ...edges2];
+        // console.log(currentNode, currentNodeEdges);
 
         for (const edge of currentNodeEdges) {
-            const newCost = totalCost(currentNode) + edge.weight;
+            const newCost = totalCost(currentNode) + edge.weight + currentNodeEdges.length;
             const neighbor = nodesByName[currentNode.name === edge.node1 ? edge.node2 : edge.node1];
 
             // Проверяем если новый путь до соседа короче или соседа еще нет в открытом списке
